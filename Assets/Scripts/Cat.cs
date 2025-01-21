3e6b3678d3093ed9a5fffc776f77223b968a2cd5
KSangMin <fhfpszmfnwm@naver.com>
2025-01-21 12:47:19 +0900

    고양이 먹이 먹는 기능 구현
    고양이 먹이 먹는 기능 구현

---
+++ Assets/Scripts/Cat.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    public GameObject full;
+
+    public float speed = 20f;
+
+    private float _fullEnergy = 5f;
+    private float _energy = 0f;
+    public RectTransform foreground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float x = Random.Range(-9f, 9f);
+        transform.position = new Vector3(x, 31f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_energy < _fullEnergy)
+        {
+            transform.position += Vector3.down * Time.deltaTime * speed;
+        }
+        else
+        {
+            if (transform.position.x > 1)
+            {
+                transform.position += Vector3.right * Time.deltaTime * speed * 1.5f;
+            }
+            else
+            {
+                transform.position += Vector3.left * Time.deltaTime * speed * 1.5f;
+
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Food"))
+        {
+            if(_energy < _fullEnergy)
+            {
+                _energy += 1f;
+                foreground.localScale = new Vector3(_energy / _fullEnergy, 1f, 1f);
+                if(_energy >= _fullEnergy)
+                {
+                    full.SetActive(true);
+                }
+            }
+        }
+    }
+}
